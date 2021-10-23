4d13a16202b308f1524b5394ab24d6f372c5998f
vvandriani <andrianififi75@gmail.com>
2021-10-23 13:30:36 +0700

    Upload file

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1>Perkenalkan</h1>";
+		echo"Nama saya Vivi Andriani Rukmana dari Prodi Sistem Informasi kelas 12.3A.15";
+	}
+}
