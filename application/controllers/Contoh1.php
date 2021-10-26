53e083dd8cc895557b80dfa859d2869730f199fe
vvandriani <andrianififi75@gmail.com>
2021-10-26 15:37:25 +0700

    first commit

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -3,7 +3,7 @@ class Contoh1 extends CI_Controller
 {
 	public function index()
 	{
-		echo "<h1>Perkenalkan</h1>";
-		echo"Nama saya Vivi Andriani Rukmana dari Prodi Sistem Informasi kelas 12.3A.15";
+		echo "<h1>Perkenalkan<h1>";
+		echo "Nama saya Vivi Andriani Rukmana dari kelas 12.3A.15 Program Studi Sistem Informasi";
 	}
 }
